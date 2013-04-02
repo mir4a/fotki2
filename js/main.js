91f5833b3d063bc38a95ae35e646f0ed471c4d99
mir4a <marty.mir4a@gmail.com>
2013-04-02 21:09:42 +0300

    try recursive begin

--- js/main.js
+++ js/main.js
@@ -1,41 +1,82 @@
-var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/'}, function(data){
-//var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/updated;2013-03-19T17:41:11Z,619984,28797441/?limit=100'}, function(data){
-//var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/updated;2013-03-19T11:14:54Z,103539,28797441/?limit=100'}, function(data){
-//var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/updated;2013-03-19T09:41:53Z,51293,28797441/?limit=100'}, function(data){
-    console.info('Ajax req done');
-}, 'html');
+var entries = [],
+    album_url = 'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/';
 
-jqxhr
-    .promise()
-    .done(function(data){
-    console.info('done');
-//    console.dir(data);
-})
-    $.when(jqxhr).then(function(data,textStatus,jqXHR){
-        console.warn('done then');
-        console.info(jqXHR.status);
-        var xml = data,
-            json = $.xml2json(xml)
-            link = json.link,
-            $ent = json.entry,
-            img_count = json.image_count['value'];
+function loadFeed(url) {
+    var d = $.Deferred();
+    var jqxhr = $.get('proxy.php',{site: url}, function(data){
+        console.info('Ajax req done');
+    }, 'html');
 
-        if (link[2]) {
-            var next = link[2]['href'];
-            alert(link[2]['rel']);
-        } else {
-            alert(link[1]['rel']);
-        }
+    jqxhr
+        .fail(function(){
+        alert('Что пошло не так');
+    })
+        .done(function(data){
+            var xml = data,
+                json = $.xml2json(xml)
+                link = json.link,
+                $ent = json.entry,
+                img_count = json.image_count['value'];
+            alert(img_count);
+            console.log(json);
+            console.log(link);
 
-        alert(img_count);
-        console.log(json);
-        console.log(link);
-        initGallery($ent);
+            for (var i=0;i<$ent.length;i++) {
+                entries.push($ent[i]);
+            }
+
+
+            if (img_count > 100) {
+                var linkNext = link[2].href;
+            } else {
+                var linkNext = null;
+            }
+
+
+            d.resolve(linkNext);
+        });
+
+    return d.promise();
+}
+
+var t = loadFeed(album_url).done(function(arg){
+    alert('Defff');
+    console.dir(entries);
+    console.log(arg);
+    loadFeed(arg);
+}).fail(function(){
+        alert('Fail');
     });
 
-function initGallery(items) {
-
-    for (var i=0; i<items.length; i++) {
-        console.dir(items[i].img);
-    }
-}
+//    .done(function(data){
+//    console.info('done');
+////    console.dir(data);
+//})
+//    $.when(jqxhr).then(function(data,textStatus,jqXHR){
+//        console.warn('done then');
+//        console.info(jqXHR.status);
+//        var xml = data,
+//            json = $.xml2json(xml)
+//            link = json.link,
+//            $ent = json.entry,
+//            img_count = json.image_count['value'];
+//
+//        if (link[2]) {
+//            var next = link[2]['href'];
+//            alert(link[2]['rel']);
+//        } else {
+//            alert(link[1]['rel']);
+//        }
+//
+//        alert(img_count);
+//        console.log(json);
+//        console.log(link);
+//        initGallery($ent);
+//    });
+//
+//function initGallery(items) {
+//
+//    for (var i=0; i<items.length; i++) {
+//        console.dir(items[i].img);
+//    }
+//}

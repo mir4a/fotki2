35223e8d1527f9cd96b50530fda00caaed9076df
mir4a <marty.mir4a@gmail.com>
2013-04-02 20:39:04 +0300

    before deferred

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-
 var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/'}, function(data){
+//var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/updated;2013-03-19T17:41:11Z,619984,28797441/?limit=100'}, function(data){
+//var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/updated;2013-03-19T11:14:54Z,103539,28797441/?limit=100'}, function(data){
+//var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/updated;2013-03-19T09:41:53Z,51293,28797441/?limit=100'}, function(data){
     console.info('Ajax req done');
 }, 'html');
 
@@ -13,6 +15,27 @@ jqxhr
         console.warn('done then');
         console.info(jqXHR.status);
         var xml = data,
-            json = $.xml2json(xml);
+            json = $.xml2json(xml)
+            link = json.link,
+            $ent = json.entry,
+            img_count = json.image_count['value'];
+
+        if (link[2]) {
+            var next = link[2]['href'];
+            alert(link[2]['rel']);
+        } else {
+            alert(link[1]['rel']);
+        }
+
+        alert(img_count);
         console.log(json);
+        console.log(link);
+        initGallery($ent);
     });
+
+function initGallery(items) {
+
+    for (var i=0; i<items.length; i++) {
+        console.dir(items[i].img);
+    }
+}

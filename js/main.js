4dbfb058a5d6202b398f805af7715a91a4cce1f0
mir4a <marty.mir4a@gmail.com>
2013-04-03 00:29:58 +0300

    find current image from url

--- js/main.js
+++ js/main.js
@@ -1,9 +1,72 @@
 var entries = [],
+    step,
     author = 'aig1001',
     album = '63684',
-    album_url = 'http://api-fotki.yandex.ru/api/users/miroslav-martynoff/album/127195/photos/';
+    appid = '55577',
+    api_url = 'http://api-fotki.yandex.ru/api/users/',
+//    album_url = 'http://api-fotki.yandex.ru/api/users/miroslav-martynoff/album/127195/photos/';
 //    album_url = 'http://api-fotki.yandex.ru/api/users/miroslav-martynoff/album/127192/photos/';
-//    album_url = 'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/';
+    album_url = api_url + author + '/album/' + album + '/photos/';
+
+function parseHash(hash) {
+    var d1 = $.Deferred(),
+        str = hash.slice(1),
+        ar = str.split(':');
+
+        if (ar.length === 4) {
+            return d1.resolve(ar);
+        } else {
+            return d1.reject();
+        }
+    return d1.promise();
+}
+
+function checkUrl() {
+    var docLoc = document.location,
+        docLocPr = docLoc['protocol'],
+        docLocHs = docLoc['host'],
+        docLocP = docLoc['pathname'],
+        docLocHash = docLoc['hash'];
+
+    parseHash(docLocHash)
+        .done(function(ar){
+            console.log('ar===' + ar);
+            console.dir(ar);
+            var _appid = ar[0],
+                _author = ar[1],
+                _album = ar[2],
+                _start = parseInt(ar[3]),
+                gen_url = album_url;
+
+            if (_appid === appid) {
+                alert('app id correct!');
+                gen_url = api_url + _author + '/album/' + _album + '/photos/';
+            } else {
+                alert('wrong!!! app id!');
+                alert(gen_url);
+                _start = 0;
+            }
+
+            alert('start='+_start);
+
+            $.when(loadStatus(gen_url)).done(function(){
+                console.log('%%%%%%%%%%');
+                slider(entries, step, _start);
+            });
+        })
+        .fail(function(){
+            alert('hash a little');
+            var newLoc = docLocPr + '//' + docLocHs + docLocP + '#' + appid + ':' + author + ':' + album + ':' + 0;
+            document.location = newLoc;
+
+            $.when(loadStatus(album_url)).done(function(){
+                console.log('%%%%%%%%%%');
+                slider(entries, step, 0);
+            });
+        });
+}
+checkUrl();
+
 
 function loadFeed(url) {
     var d = $.Deferred();
@@ -19,23 +82,18 @@ function loadFeed(url) {
                 link = json.link,
                 $ent = json.entry,
                 img_count = json.image_count;
-            alert(img_count);
             console.log(json);
             console.log(link);
-//            console.log(data.status);
 
             for (var i=0;i<$ent.length;i++) {
                 entries.push($ent[i]);
             }
-
-
 
             if (link.length === 3) {
                 var linkNext = link[2].href;
             } else {
                 var linkNext = undefined;
             }
-
 
             d.resolve(linkNext);
         });
@@ -49,44 +107,41 @@ function loadStatus(url) {
             if (linkNext === undefined) {
                 return linkNext;
             }
-
             return loadStatus(linkNext);
         });
-
 }
 
 
 
-$.when(loadStatus(album_url)).done(function(){
-    console.log('%%%%%%%%%%');
-//    console.dir(entries);
-    slider(entries);
-});
 
-function slider(items, step) {
+function slider(items, step, start) {
     step = step || 10;
-    var docLoc = document.location;
-    var docLocPr = document.location['protocol'];
-    var docLocHs = document.location['host'];
-    var docLocP = document.location['pathname'];
-    var newLoc = docLocPr + '//' + docLocHs + docLocP + '#author=' + author + '&album=' + album + '&slide=' + '0';
-    document.location = newLoc;
-
-//    history.pushState(newLoc);
-//    while (step)
+    start = start || 0;
 
     for (var i=0;i<step;i++) {
-
-        if (i < items.length) {
-            var img = items[i].img,
-                xl = img[3].href;
-
+        var curI = parseInt(start + i);
+        if (curI < items.length) {
+            console.warn('iterator == ' + i + '; current image index (items length =' + items.length + ') == ' + curI);
+            console.warn('number of images: ' + items[curI].img.length);
+            var img = items[curI].img,
+                xxxs = img[4].href,
+                xxs = img[0].href,
+                xs = img[6].href,
+                s = img[5].href,
+                m = img[2].href,
+                l = img[3].href,
+                xl = img[1].href;
+            console.log('фотки размера xxxs: '+xxxs);
+            console.log('фотки размера xxs: '+xxs);
+            console.log('фотки размера xs: '+xs);
+            console.log('фотки размера s: '+s);
+            console.log('фотки размера m: '+m);
+            console.log('фотки размера l: '+l);
             console.log('фотки размера xl: '+xl);
+//            console.log('фотки размера xxs: '+xxs);
+            console.log('length of items='+items.length);
+            console.error('curI='+curI);
+            console.log('step='+i);
         }
-
-        console.log('step='+i);
-
-
     }
-    console.dir(docLoc);
 }

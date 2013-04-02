2d3b02bf54afca3abbb333fcea980dd232778a5c
mir4a <marty.mir4a@gmail.com>
2013-04-02 18:10:52 +0300

    first commit

---
+++ proxy.php
@@ -0,0 +1,10 @@
+<?php
+if(isset($_GET['site'])){
+    $f = fopen($_GET['site'], 'r');
+    $html = '';
+    while (!feof($f)) {
+        $html .= fread($f, 24000);
+    }
+    fclose($f);
+    echo $html;
+}

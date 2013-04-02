a241c3a957cd4cb2d99af6f730b5b82a664b0bc4
mir4a <marty.mir4a@gmail.com>
2013-04-02 18:27:40 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,59 +1,18 @@
 
 var jqxhr = $.get('proxy.php',{site:'http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/'}, function(data){
-//    var output = '';
-    var xml = data;
-//        xmlDoc = $.parseXML(xml),
-//        $xml = $(xmlDoc),
-//        items = $xml.find('item');
-
-
-//        console.log(output);
-//    wrap.append(output);
-//        console.log(xml);
-//    var href = $(data).find('.hdtitle').first().children(':first-child').prop('href');
-//    var url = href.split('/');
-//    href = href.replace(url[2], 'nu.nl');
-//
-//    // Put the 'href' inside your div as a link
-//    $('#myDiv').html('<a href="' + href + '" target="_blank">' + href + '</a>');
-alert('start');
-//    return xml;
-
+    console.info('Ajax req done');
 }, 'html');
 
 jqxhr
     .promise()
     .done(function(data){
-    alert('done');
+    console.info('done');
 //    console.dir(data);
 })
     $.when(jqxhr).then(function(data,textStatus,jqXHR){
-        alert('done then');
+        console.warn('done then');
         console.info(jqXHR.status);
-//        console.trace(jqXHR);
-//        console.time();
-//        console.warn(data);
         var xml = data,
-//            xmlDoc = $.parseXML(xml),
-//            xml = $(xmlDoc),
-//            $items = $xml.find('entry'),
             json = $.xml2json(xml);
-//            $items = $json.entry;
-//            $f = $items.find("f");
-
-//        console.log(xmlDoc);
         console.log(json);
-//        console.log($f);
-//        console.dir($items);
     });
-//    .done(function(data){
-//                alert('done');
-//        console.dir(data);
-//    })
-//    .fail(function(){
-//                alert('fail');
-//    })
-//    .always(function(){
-//                alert('always');
-////                overlay.show(250);
-//    });
